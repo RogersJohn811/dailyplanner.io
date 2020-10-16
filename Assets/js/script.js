4d542020a353430a66d60410c42613a65536c6df
RogersJohn811 <RogersJohn811@gmail.com>
2020-10-16 16:21:38 -0400

    First commit

---
+++ Assets/js/script.js
@@ -0,0 +1,41 @@
+// get our document ready for jQuery.
+
+// Declare our time and date variables using moment.js
+
+// Store our necessary DOM elements to variables.
+
+// declare our global variables.
+
+// Place our current date in the jumbotron in the UI.
+
+ /*
+        colorCode() function determines if a timeslot is past,
+        future, or present and colors the block accordingly.
+        Red ("#ff7675") will appear for the current time block.
+        Green ("#55efc4") will appear for future time blocks.
+        Previous time blocks stay the default grey.
+*/
+
+// Determine which colors to show for each times lot.
+
+// if the current time is equal to a timeblock time make it green.
+
+// if the current time is less than a timeblock time make it red.
+
+// Cycle through local storage, if an entry exists show it in the 
+// correct timeslot.
+
+// Define what happens when our user clicks the "saveBtn"
+
+// Stop our submit button from deleting the textarea data.
+
+// Save the inputBox value.
+
+// Get the timeslot value which corresponds to the time.
+
+// store our inputBox and timeSlot vars in global variables.
+
+// Log to the console which timeslot was just saved.
+
+// Update localStorage to include our recently input values.
+
